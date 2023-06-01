3fb1bdf9adaf601c5bf0b314a906234c1dfb0d5f
linonetwo <linonetwo012@gmail.com>
2023-06-01 16:35:00 +0800

    Updated with Git-Sync

--- scripts/download-installers.mjs
+++ scripts/download-installers.mjs
@@ -1,11 +1,16 @@
 import fs from 'fs';
 import path from 'path';
-import { mkdir, writeFile } from "fs/promises";
-import { Readable } from "stream";
-import { finished } from "stream/promises";
-import { backOff } from "exponential-backoff";
+import { mkdir, writeFile, unlink } from 'fs/promises';
+import { Readable } from 'stream';
+import { finished } from 'stream/promises';
+import { backOff } from 'exponential-backoff';
 
-const latestReleaseData = await fetch('https://api.github.com/repos/tiddly-gittly/TidGi-Desktop/releases/latest').then(async (response) => await response.json());
+$.env.HTTP_PROXY = 'http://localhost:1080';
+$.env.HTTPS_PROXY = 'http://localhost:1080';
+
+const latestReleaseData = await fetch('https://api.github.com/repos/tiddly-gittly/TidGi-Desktop/releases/latest').then(
+  async (response) => await response.json(),
+);
 const latestVersion = latestReleaseData.tag_name.replace('v', '');
 const urls = latestReleaseData.assets.map((asset) => asset.browser_download_url);
 console.log(urls);
@@ -14,14 +19,15 @@ console.log(urls);
 async function downloadAsset(asset) {
   const fileName = asset.name.replace(latestVersion, 'latest');
   console.log(`Downloading ${fileName} from ${asset.browser_download_url}`);
+  try {
+    await unlink(destination);
+  } catch {}
   const { body } = await fetch(asset.browser_download_url);
   const destination = path.join(__dirname, `../files/${fileName}`);
   const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
-  await finished((body).pipe(fileStream));
+  await finished(body.pipe(fileStream));
   console.log(`Done ${fileName}`);
 }
 
 await mkdir(path.join(__dirname, '../files'), { recursive: true });
-await Promise.all(
-  latestReleaseData.assets.map(asset => backOff(() => downloadAsset(asset))),
-);
+await Promise.all(latestReleaseData.assets.map((asset) => backOff(() => downloadAsset(asset))));

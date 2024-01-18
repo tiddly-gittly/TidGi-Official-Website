137516c09a1f4acd148cc4592f559e1d5ab4ea99
lin onetwo <linonetwo012@gmail.com>
2024-01-18 19:37:09 +0800

    fix: proxy and retry

--- scripts/download-installers.mjs
+++ scripts/download-installers.mjs
@@ -5,6 +5,7 @@ import Promise from 'bluebird';
 import { Readable } from 'stream';
 import { finished } from 'stream/promises';
 import { backOff } from 'exponential-backoff';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
 require('dotenv').config();
 const { GITHUB_TOKEN } = process.env;
@@ -13,8 +14,10 @@ const { GITHUB_TOKEN } = process.env;
 set HTTP_PROXY =http://localhost:1080
 set HTTPS_PROXY=http://localhost:1080
 */
+// useless:
 // $.env.HTTP_PROXY = 'http://127.0.0.1:1080';
 // $.env.HTTPS_PROXY = 'http://127.0.0.1:1080';
+const agent = new HttpsProxyAgent('http://127.0.0.1:1080');
 
 const latestDesktopReleaseData = await fetch('https://api.github.com/repos/tiddly-gittly/TidGi-Desktop/releases/latest').then(
   async (response) => await response.json(),
@@ -44,24 +47,34 @@ async function downloadAsset(asset, rename) {
   if (GITHUB_TOKEN) {
     headers.Authorization = `token ${GITHUB_TOKEN}`;
   }
-  const { body } = await fetch(asset.url, { headers });
+  const { body } = await fetch(asset.url, { headers, agent });
   const destination = path.join(__dirname, `../files/${fileName}`);
   const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
   await finished(body.pipe(fileStream));
   console.log(`Done ${fileName}`);
 }
 
+let chunkCounter = 0;
+
 await mkdir(path.join(__dirname, '../files'), { recursive: true });
 await Promise.all([
   ...latestDesktopReleaseData.assets.map(async (asset) => {
-    await Promise.delay(10000 * Math.random());
-    await backOff(async () => {
-      console.log(`backoff retry ${asset.name}`);
-      await downloadAsset(asset, (name) => {
-        const fileName = name.replace(latestDesktopVersion, 'latest');
-        return fileName;
-      });
-    });
+    chunkCounter += 1;
+    if (chunkCounter > latestDesktopReleaseData.assets.length / 2) {
+      await Promise.delay(20000 * Math.random());
+    } else {
+      await Promise.delay(5000 * Math.random());
+    }
+    await backOff(
+      async () => {
+        console.log(`backoff retry ${asset.name}`);
+        await downloadAsset(asset, (name) => {
+          const fileName = name.replace(latestDesktopVersion, 'latest');
+          return fileName;
+        });
+      },
+      { numOfAttempts: 10000 },
+    );
   }),
   ...latestMobileReleaseData.assets.map((asset) =>
     backOff(() =>
